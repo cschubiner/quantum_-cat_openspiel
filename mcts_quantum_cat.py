51372a090f5f59606ade374451b4b799c3d8037a
clayschubiner <cschubiner@gmail.com>
2025-01-20 14:14:11 -0500

    refactor: Adjust MCTS simulation parameters and remove result logging

--- mcts_quantum_cat.py
+++ mcts_quantum_cat.py
@@ -15,7 +15,7 @@ from open_spiel.python.algorithms.mcts import RandomRolloutEvaluator
 from open_spiel.python.games import quantum_cat
 
 
-NUM_RANDOM_BOTS = 3
+NUM_RANDOM_BOTS = 2
 
 def main():
     game = pyspiel.load_game("python_quantum_cat", {"players": 1 + NUM_RANDOM_BOTS})
@@ -31,7 +31,8 @@ def main():
             game=game,
             evaluator=ismcts_evaluator,
             uct_c=2.0,
-            max_simulations=500,
+            # max_simulations=500,
+            max_simulations=2200,
             max_world_samples=UNLIMITED_NUM_WORLD_SAMPLES,
             random_state=np.random.RandomState(999),
             final_policy_type=ISMCTSFinalPolicyType.MAX_VISIT_COUNT,
@@ -88,22 +89,6 @@ def main():
             print(f"  90% confidence interval: {mean_return:.3f} ± {plus_minus_ci:.3f}")
             
         print(f"Game over. Returns: {final_returns}")
-
-    mean_return = np.mean(ismcts_returns)
-    std_return = np.std(ismcts_returns)
-    
-    # Calculate confidence interval for the true mean (if played infinite games)
-    confidence_interval = stats.t.interval(
-        confidence=0.90,  # 90% CI => 5th to 95th percentile
-        df=len(ismcts_returns)-1,
-        loc=mean_return,
-        scale=stats.sem(ismcts_returns)
-    )
-    plus_minus_ci = (confidence_interval[1] - confidence_interval[0]) / 2
-    
-    print(f"ISMCTS results over {num_episodes} episodes:")
-    print(f"  Average return: {mean_return:.3f} ± {std_return:.3f}")
-    print(f"  90% confidence interval for true mean: {mean_return:.3f} ± {plus_minus_ci:.3f}")
 
 
 if __name__ == "__main__":
